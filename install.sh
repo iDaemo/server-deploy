a2488dafd7568667aacb1f4dc27e064d28206055
iDaemo <37049068+iDaemo@users.noreply.github.com>
2023-05-20 22:25:27 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+### upgrade everything
+sudo apt update && sudo apt upgrade -y
+
+### messing with ssh
 sudo sed -i "/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes" /etc/ssh/sshd_config
+#sudo sed -i "/^[^#]*ClientAliveInterval[[:space:]]0/c\ClientAliveInterval 60" /etc/ssh/sshd_config
+echo 'ClientAliveInterval 60' | sudo tee -a /etc/ssh/sshd_config
 sudo service sshd restart
-sudo apt update && sudo apt upgrade
+
+### create swap file edit the size 1g=2g 2g=2g 4g=4g
+sudo fallocate -l 2G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+cp /etc/fstab /etc/fstab_$(date +%Y%m%d%H%M%S)
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+echo 'vm.swappiness=10' | sudo tee -a /etc/sysctl.conf
+
+sed -i '/^#PasswordAuthentication/s/yes/no/' /etc/ssh/sshd_config
