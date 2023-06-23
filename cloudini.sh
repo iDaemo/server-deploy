da6e0cf79ce06372e981300de1d048e353a480b8
iDaemo <37049068+iDaemo@users.noreply.github.com>
2023-06-23 20:16:42 +0700

    Update cloudini.sh

--- cloudini.sh
+++ cloudini.sh
@@ -4,9 +4,9 @@ sudo apt update -y && sudo apt upgrade -y
 apt install cron
 apt install iputils-ping 
 apt install cron
-sudo useradd idaemon
-echo "idaemon:thaigaming" | sudo chpasswd
-usermod -aG sudo idaemon
+#sudo useradd idaemon
+echo "$USER:thaigaming" | sudo chpasswd
+#usermod -aG sudo idaemon
 sudo sed -i "/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes" /etc/ssh/sshd_config
 sudo service sshd restart
 echo "   "
