1413416cac92790dbb6d4a3f921560927a8f6433
iDaemo <37049068+iDaemo@users.noreply.github.com>
2023-06-23 15:09:35 +0700

    Create cyberpanel.sh

---
+++ cyberpanel.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo timedatectl set-timezone Asia/Bangkok
+sudo apt update -y && sudo apt upgrade -y
+apt install cron
+apt install iputils-ping 
+apt install cron
+sudo useradd idaemon
+echo "idaemon:thaigaming" | sudo chpasswd
+usermod -aG sudo idaemon
+sudo sed -i "/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes" /etc/ssh/sshd_config
+sudo service sshd restart
+echo "   "
+echo " DONE "
+sh <(curl https://cyberpanel.net/install.sh -v ols -p thaigaming -a -m)
