e8107e7fc9c7f5043642a59181aca51c5c1fa39f
iDaemo <37049068+iDaemo@users.noreply.github.com>
2023-06-23 11:58:46 +0700

    Update cloudini.sh

--- cloudini.sh
+++ cloudini.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 sudo timedatectl set-timezone Asia/Bangkok
 sudo apt update -y && sudo apt upgrade -y
+apt install cron
+apt install iputils-ping 
+apt install cron
 sudo useradd idaemon
 echo "idaemon:thaigaming" | sudo chpasswd
 usermod -aG sudo idaemon
