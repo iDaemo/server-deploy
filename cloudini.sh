2d0cd47500fd2f11008548441af74b4cc6427a4c
iDaemo <37049068+iDaemo@users.noreply.github.com>
2023-06-23 11:16:14 +0700

    Update cloudini.sh

--- cloudini.sh
+++ cloudini.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo timedatectl set-timezone Asia/Bangkok
 sudo apt update -y && sudo apt upgrade -y
-useradd idaemon
-echo thaigaming | passwd --stdin idaemon
+sudo useradd idaemon
+sudo echo thaigaming | passwd --stdin idaemon
 usermod -aG sudo idaemon
 sudo sed -i "/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes" /etc/ssh/sshd_config
 sudo service sshd restart
