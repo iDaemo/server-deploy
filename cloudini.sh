0148a5f6ed3405afeffa8d0d750f831f152258b0
iDaemon <37049068+iDaemo@users.noreply.github.com>
2023-11-20 05:00:24 +0700

    Update cloudini.sh

--- cloudini.sh
+++ cloudini.sh
@@ -8,8 +8,14 @@ echo "$USER:thaigaming" | sudo chpasswd
 sudo sed -i "/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes" /etc/ssh/sshd_config
 #sudo service sshd restart
 
+#prepare server
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install cron
+sudo apt install iputils-ping 
+sudo apt install nano
+
 #update source maraidb
-sudo apt-get install apt-transport-https curl
+sudo install apt-transport-https curl
 sudo mkdir -p /etc/apt/keyrings
 sudo curl -o /etc/apt/keyrings/mariadb-keyring.pgp 'https://mariadb.org/mariadb_release_signing_key.pgp'
 tee -a /etc/apt/sources.list.d/mariadb.sources <<EOF
@@ -23,10 +29,6 @@ EOF
 
 #Essential setup
 sudo timedatectl set-timezone Asia/Bangkok
-sudo apt update -y && sudo apt upgrade -y
-sudo apt install cron
-sudo apt install iputils-ping 
-sudo apt install nano
 sudo apt autoremove -y
 sudo shutdown -r now
 
