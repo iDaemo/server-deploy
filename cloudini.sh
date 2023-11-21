c0ab397d6642ce21beec8b9d9cfee4575ab1e893
iDaemon <37049068+iDaemo@users.noreply.github.com>
2023-11-21 22:14:02 +0700

    Update cloudini.sh

--- cloudini.sh
+++ cloudini.sh
@@ -17,12 +17,7 @@ sudo apt install cron
 sudo apt install iputils-ping 
 sudo apt install nano
 sudo apt install apt-utils -y
-sudo apt install software-properties-common gnupg2 -y
-sudo apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
-#mariadb10.8
-#sudo add-apt-repository 'deb [arch=amd64] http://mariadb.mirror.globo.tech/repo/10.8/ubuntu jammy main'
-#mariadb11.1
-sudo add-apt-repository 'deb [arch=amd64] https://mirror.djvg.sg/mariadb/repo/11.1/ubuntu jammy main'
+sudo curl -LsS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash -s -- --mariadb-server-version=11.1
 
 ### create swap file edit the size 1g=2g 2g=2g 4g=4g
 sudo fallocate -l 2G /swapfile
@@ -34,9 +29,9 @@ echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 echo 'vm.swappiness=10' | sudo tee -a /etc/sysctl.conf
 
 #unattendence 
-sudo sed -i.bak '/^APT::Periodic::Update-Package-Lists/ s/"0"/"1"/' /etc/apt/apt.conf.d/20auto-upgrades
-sudo sed -i.bak '/^APT::Periodic::Unattended-Upgrade/ s/"0"/"1"/' /etc/apt/apt.conf.d/20auto-upgrades
-sudo dpkg-reconfigure -f noninteractive unattended-upgrades
+#sudo sed -i.bak '/^APT::Periodic::Update-Package-Lists/ s/"0"/"1"/' /etc/apt/apt.conf.d/20auto-upgrades
+#sudo sed -i.bak '/^APT::Periodic::Unattended-Upgrade/ s/"0"/"1"/' /etc/apt/apt.conf.d/20auto-upgrades
+#sudo dpkg-reconfigure -f noninteractive unattended-upgrades
 
 
 #start to install thing
@@ -47,7 +42,7 @@ sudo dpkg-reconfigure -f noninteractive unattended-upgrades
 sudo apt update -y
 export DEBIAN_FRONTEND=noninteractive
 export DEBIAN_PRIORITY=critical
-sudo apt -qy -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" dist-upgrade
+yes '' | sudo apt-y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
 
 sudo apt autoclean -y && sudo apt autoremove -y
 sudo shutdown -r now
